db7b23bf119d465cbade9717fff3ca2271b8dc14
Wang CHEN <solid.chen9@gmail.com>
2022-12-11 16:19:28 +0100

    done from toto

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      How a user interacts with and experiences your website will determine your
+      impact. Utility, ease of use, and efficiency are key.
+    </p>
+    <p>
+      How a user interacts with and experiences your website will determine your
+      impact. Utility, ease of use, and efficiency are key.
+    </p>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/ninja?name=mario', {
+  method: 'post',
+  body: { age: 30 },
+});
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      How a user interacts with and experiences your website will determine your
+      impact. Utility, ease of use, and efficiency are key.
+    </p>
+    <p>
+      How a user interacts with and experiences your website will determine your
+      impact. Utility, ease of use, and efficiency are key.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
